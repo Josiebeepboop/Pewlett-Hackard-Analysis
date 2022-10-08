5bc821ab2511cf1f9ea809ca71c06fb19aa8c5ba
josiebeepboop <josette.bitanga@gmail.com>
2022-10-07 22:06:54 -0400

    deliverable 2

--- Employee_Database_challenge.sql
+++ Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Create a retirement titles table which holds employees born between 1952 and 1955
 SELECT e.emp_no,e.first_name,e.last_name,t.title,t.from_date,t.to_date
 INTO retirement_titles
 FROM Employees as e
@@ -16,8 +17,21 @@ FROM retirement_titles
 WHERE to_date = '9999-01-01'
 ORDER BY emp_no, to_date DESC;
 
+-- Create table with only unique titles with count of employees holding that title
 SELECT COUNT(titles) as 'Title Count',title
 FROM unique_titles
---INTO retiring_titles
+INTO retiring_titles
 GROUP BY title
 ORDER BY 'Title Count' DESC;
+
+-- Create a Mentorship Eligibility table that holds employees who are eligible for mentorship
+SELECT DISTINCT ON(e.emp_no)e.emp_no,e.first_name,e.last_name,e.birth_date,de.from_date,de.to_date,t.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON e.emp_no = de.emp_no
+INNER JOIN titles as t
+ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no,t.to_date DESC;
